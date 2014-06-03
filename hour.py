eb2f9b044ff708e795c568f145d63d888975d355
Valentin Brossard <valentin.brossard@gmail.com>
2014-06-03 21:25:16 +0200

    Class for time

--- hour.py
+++ hour.py
@@ -1,5 +1,12 @@
+import time
+import os
+
 class Hour:
     "Manage Hour"
 
     def update(self):
         print "Update !"
+
+    def print_time(self):
+        os.system("clear")
+        print time.strftime("%H:%M")
